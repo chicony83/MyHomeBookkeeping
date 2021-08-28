c72f32a64c3278bc1c2a25dfc7076da21025afc6
chicony83 <chicony83@mail.com>
2021-08-28 21:21:55 +0300

    fragment added

---
+++ app/src/main/java/com/chico/myhomebookkeeping/ui/timePeriod/TimePeriodFragment.kt
@@ -0,0 +1,29 @@
+package com.chico.myhomebookkeeping.ui.timePeriod
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.chico.myhomebookkeeping.databinding.FragmentNewMoneyMovingBinding
+import com.chico.myhomebookkeeping.databinding.FragmentPeriodTimeBinding
+
+class TimePeriodFragment:Fragment() {
+    private var _binding: FragmentPeriodTimeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPeriodTimeBinding.inflate(inflater, container, false)
+
+        return binding.root
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+}
