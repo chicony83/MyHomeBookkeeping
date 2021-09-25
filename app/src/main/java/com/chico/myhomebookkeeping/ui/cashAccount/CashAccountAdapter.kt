a70b8896df57c4587efbde5edf0d3342a68b3c70
chicony83 <chicony83@mail.com>
2021-09-25 13:00:49 +0300

    hide card number when it empty

--- app/src/main/java/com/chico/myhomebookkeeping/ui/cashAccount/CashAccountAdapter.kt
+++ app/src/main/java/com/chico/myhomebookkeeping/ui/cashAccount/CashAccountAdapter.kt
@@ -1,26 +1,37 @@
 package com.chico.myhomebookkeeping.ui.cashAccount
 
+import android.annotation.SuppressLint
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.chico.myhomebookkeeping.R
 import com.chico.myhomebookkeeping.`interface`.OnItemViewClickListener
 import com.chico.myhomebookkeeping.databinding.RecyclerViewItemCashAccountBinding
 import com.chico.myhomebookkeeping.db.entity.CashAccount
+import com.chico.myhomebookkeeping.helpers.UiHelper
 
 class CashAccountAdapter(
     private var cashAccountList: List<CashAccount>,
     val listener: OnItemViewClickListener
 ) :
     RecyclerView.Adapter<CashAccountAdapter.ViewHolder>() {
-
+    val uiHelper = UiHelper()
+    private lateinit var context: Context
+    private lateinit var textCardNumber: String
     override fun onCreateViewHolder(
         parent: ViewGroup,
         viewType: Int,
     ): ViewHolder {
         val binding = RecyclerViewItemCashAccountBinding
             .inflate(LayoutInflater.from(parent.context), parent, false)
-
+        context = parent.context
+        getStrings()
         return ViewHolder(binding)
+    }
+
+    private fun getStrings() {
+        textCardNumber = context.getString(R.string.sign_number_of_account)
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
@@ -34,15 +45,16 @@ class CashAccountAdapter(
     ) :
         RecyclerView.ViewHolder(binding.root) {
 
+        @SuppressLint("SetTextI18n")
         fun bind(cashAccount: CashAccount) {
             with(binding) {
                 nameCashAccount.text = cashAccount.accountName
 
-                if (cashAccount.bankAccountNumber.toString().isBlank()){
-                    numberCashAccount.text = ""
-                }
-                else{
-                    numberCashAccount.text = cashAccount.bankAccountNumber.toString()
+                if (cashAccount.bankAccountNumber.isEmpty()) {
+                    uiHelper.hideUiElement(numberCashAccount)
+                    numberCashAccount.text = null
+                } else {
+                    numberCashAccount.text = "$textCardNumber  ${cashAccount.bankAccountNumber}"
                 }
                 cashAccountItem.setOnClickListener {
                     cashAccount.cashAccountId?.let { it1 -> listener.onClick(it1) }
@@ -50,6 +62,7 @@ class CashAccountAdapter(
             }
         }
     }
+
 }
 
 
