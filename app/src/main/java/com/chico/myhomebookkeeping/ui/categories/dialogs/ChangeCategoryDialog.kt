1820f122aa6327b1dc890e9f526a29a57b1c08d7
chicony83 <chicony83@gmail.com>
2021-11-29 12:13:04 +0300

    add functions in ChangeCategoryDialog renames in dialog_change_category

--- app/src/main/java/com/chico/myhomebookkeeping/ui/categories/dialogs/ChangeCategoryDialog.kt
+++ app/src/main/java/com/chico/myhomebookkeeping/ui/categories/dialogs/ChangeCategoryDialog.kt
@@ -3,6 +3,9 @@ package com.chico.myhomebookkeeping.ui.categories.dialogs
 import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
 import androidx.fragment.app.DialogFragment
 import com.chico.myhomebookkeeping.R
 import com.chico.myhomebookkeeping.db.entity.Categories
@@ -19,9 +22,59 @@ class ChangeCategoryDialog(
             val inflater = requireActivity().layoutInflater
             val layout = inflater.inflate(R.layout.dialog_change_category, null)
 
+            val name = layout.findViewById<EditText>(R.id.name)
+            val incomeRadioButton = layout.findViewById<RadioButton>(R.id.incomeRadioButton)
+            val spendingRadioButton = layout.findViewById<RadioButton>(R.id.spendingRadioButton)
+
+            val saveButton = layout.findViewById<Button>(R.id.saveButton)
+            val cancelButton = layout.findViewById<Button>(R.id.cancelButton)
+
+            name.setText(category?.categoryName.toString())
+
+            if (category?.isIncome == true){
+                incomeRadioButton.isChecked = true
+            }
+            if (category?.isIncome == false){
+                spendingRadioButton.isChecked = true
+            }
+
+            saveButton.setOnClickListener {
+                val isTypeOfCategorySelected =
+                    getIsTypeOfCategorySelected(incomeRadioButton, spendingRadioButton)
+                val isIncome = getTypeOfCategory(incomeRadioButton, spendingRadioButton)
+                if (isTypeOfCategorySelected) {
+                    onChangeCategoryCallBack.change(
+                        id = category?.categoriesId ?: 0,
+                        name = name.text.toString(),
+                        isIncome = isIncome
+                    )
+                }
+            }
+
+            cancelButton.setOnClickListener {
+                closeDialog()
+            }
 
             builder.setView(layout)
             builder.create()
         } ?: throw IllegalStateException(getString(R.string.exceptions_activity_cant_be_null))
+    }
+
+    private fun getTypeOfCategory(
+        incomeRadioButton: RadioButton,
+        spendingRadioButton: RadioButton
+    ): Boolean {
+        return incomeRadioButton.isChecked
+    }
+
+    private fun getIsTypeOfCategorySelected(
+        incomeRadioButton: RadioButton,
+        spendingRadioButton: RadioButton
+    ): Boolean {
+        return incomeRadioButton.isChecked or spendingRadioButton.isChecked
+    }
+
+    private fun closeDialog() {
+        dialog?.cancel()
     }
 }
