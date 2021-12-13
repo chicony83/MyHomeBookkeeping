a31cf657d57501044ee40f2f213881910cb7c694
chicony83 <chicony83@gmail.com>
2021-12-13 22:47:05 +0300

    add BlanksFragment.kt

---
+++ app/src/main/java/com/chico/myhomebookkeeping/ui/blanks/BlanksFragment.kt
@@ -0,0 +1,10 @@
+package com.chico.myhomebookkeeping.ui.blanks
+
+import androidx.fragment.app.Fragment
+import com.chico.myhomebookkeeping.databinding.FragmentBlanksBinding
+
+class BlanksFragment:Fragment() {
+    private var _binding:FragmentBlanksBinding?=null
+    private val binding get() = _binding
+
+}
