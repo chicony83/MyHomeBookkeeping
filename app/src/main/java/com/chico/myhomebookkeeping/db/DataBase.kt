2b95ea5b3aab30fbaf5ff0c12828da7c83fdac64
chicony83 <chicony83@gmail.com>
2021-12-14 16:27:56 +0300

    update DataBase.kt

--- app/src/main/java/com/chico/myhomebookkeeping/db/DataBase.kt
+++ app/src/main/java/com/chico/myhomebookkeeping/db/DataBase.kt
@@ -18,7 +18,7 @@ import com.chico.myhomebookkeeping.db.entity.*
         BlankMoneyMovement::class,
         Icons::class
     ],
-    version = 3,
+    version = 2,
     exportSchema = true,
 
     )
@@ -39,18 +39,19 @@ object dataBase {
             "DataBase"
         )
             .addMigrations(migration_1_2)
-            .addMigrations(migration_2_3)
+//            .addMigrations(migration_2_3)
             .build()
 }
 
 object migration_1_2 : Migration(1, 2) {
     override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("CREATE TABLE IF NOT EXISTS 'blanks_table' ('id' INTEGER , 'icon' INTEGER NOT NULL,'name' TEXT NOT NULL, 'cash_account' INTEGER NOT NULL, 'currency' INTEGER NOT NULL, 'category' INTEGER NOT NULL, 'description' TEXT NOT NULL, PRIMARY KEY('id'))")
-    }
-}
-
-object migration_2_3 : Migration(2, 3) {
-    override fun migrate(database: SupportSQLiteDatabase) {
         database.execSQL("CREATE TABLE IF NOT EXISTS 'icons_table' ( 'id' INTEGER , 'name' TEXT NOT NULL, 'icon_value' TEXT NOT NULL, PRIMARY KEY ('id'))")
     }
 }
+
+//object migration_2_3 : Migration(2, 3) {
+//    override fun migrate(database: SupportSQLiteDatabase) {
+//
+//    }
+//}
