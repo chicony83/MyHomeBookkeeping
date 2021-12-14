dc13ddc663a070cfa3b6a723cf10ae6f7efa775d
chicony83 <chicony83@gmail.com>
2021-12-14 20:38:32 +0300

    add new fast new moneyMovement add rating images

--- app/src/main/java/com/chico/myhomebookkeeping/ui/fastMoneyMovings/FastMoneyMovingFragment.kt
+++ app/src/main/java/com/chico/myhomebookkeeping/ui/fastMoneyMovings/FastMoneyMovingFragment.kt
@@ -5,13 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import com.chico.myhomebookkeeping.R
 import com.chico.myhomebookkeeping.databinding.FragmentFastMoneyMovingBinding
+import com.chico.myhomebookkeeping.helpers.NavControlHelper
 
-class FastMoneyMovingFragment:Fragment() {
-    private var _binding:FragmentFastMoneyMovingBinding?=null
+class FastMoneyMovingFragment : Fragment() {
+    private var _binding: FragmentFastMoneyMovingBinding? = null
     private val binding get() = _binding!!
 
-    private lateinit var fastMoneyMovingViewModel:FastMoneyMovingViewModel
+    private lateinit var control: NavController
+    private lateinit var navControlHelper: NavControlHelper
+
+    private lateinit var fastMoneyMovingViewModel: FastMoneyMovingViewModel
 
 
     override fun onCreateView(
@@ -19,8 +26,15 @@ class FastMoneyMovingFragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentFastMoneyMovingBinding.inflate(inflater,container,false)
+        _binding = FragmentFastMoneyMovingBinding.inflate(inflater, container, false)
 
+        control = activity?.findNavController(R.id.nav_host_fragment)!!
+
+        navControlHelper = NavControlHelper(controller = control)
+
+        with(binding) {
+            newBlankButton.setOnClickListener { navControlHelper.moveToSelectedFragment(R.id.nav_new_fast_money_moving_fragment) }
+        }
 
         return binding.root
     }
