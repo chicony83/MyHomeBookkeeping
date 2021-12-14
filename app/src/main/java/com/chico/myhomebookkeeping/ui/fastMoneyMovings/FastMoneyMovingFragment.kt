acde85964213abac54256aeba4c5fd10d27b05f0
chicony83 <chicony83@gmail.com>
2021-12-14 19:29:45 +0300

    some renames

--- app/src/main/java/com/chico/myhomebookkeeping/ui/fastMoneyMovings/FastMoneyMovingFragment.kt
+++ app/src/main/java/com/chico/myhomebookkeeping/ui/fastMoneyMovings/FastMoneyMovingFragment.kt
@@ -5,10 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.chico.myhomebookkeeping.databinding.FragmentBlanksBinding
+import com.chico.myhomebookkeeping.databinding.FragmentFastMoneyMovingBinding
 
 class FastMoneyMovingFragment:Fragment() {
-    private var _binding:FragmentBlanksBinding?=null
+    private var _binding:FragmentFastMoneyMovingBinding?=null
     private val binding get() = _binding!!
 
     private lateinit var fastMoneyMovingViewModel:FastMoneyMovingViewModel
@@ -19,7 +19,7 @@ class FastMoneyMovingFragment:Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        _binding = FragmentBlanksBinding.inflate(inflater,container,false)
+        _binding = FragmentFastMoneyMovingBinding.inflate(inflater,container,false)
 
 
         return binding.root
