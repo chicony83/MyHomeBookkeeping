c3028c94bde5c41e8a8d9d09638ec7272ecffb79
chicony83 <chicony83@gmail.com>
2022-01-18 18:18:09 +0100

    set images in dialog

--- app/src/main/java/com/chico/myhomebookkeeping/ui/dialogs/SelectIconDialog.kt
+++ app/src/main/java/com/chico/myhomebookkeeping/ui/dialogs/SelectIconDialog.kt
@@ -4,12 +4,18 @@ import android.app.AlertDialog
 import android.app.Dialog
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.fragment.app.DialogFragment
 import com.chico.myhomebookkeeping.R
 import com.chico.myhomebookkeeping.db.entity.IconsResource
+import com.chico.myhomebookkeeping.helpers.Message
 import java.lang.IllegalStateException
 
-class SelectIconDialog(iconsList: List<IconsResource>) : DialogFragment() {
+class SelectIconDialog(
+    private val iconsList: List<IconsResource>
+) : DialogFragment() {
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let { it ->
             val builder = AlertDialog.Builder(it)
@@ -18,11 +24,30 @@ class SelectIconDialog(iconsList: List<IconsResource>) : DialogFragment() {
             buildLayout(layout)
             builder.setView(layout)
 
+            val selectButton = layout.findViewById<Button>(R.id.selectButton)
+            val cancelButton = layout.findViewById<Button>(R.id.cancelButton)
+
+            cancelButton.setOnClickListener {
+                dialogCancel()
+            }
+
             builder.create()
         } ?: throw  IllegalStateException(getString(R.string.exceptions_activity_cant_be_null))
     }
 
-    private fun buildLayout(layout: View) {
+    private fun dialogCancel() {
+        dialog?.cancel()
+    }
 
+    private fun buildLayout(layout: View) {
+        val iconsLayout = layout.findViewById<LinearLayout>(R.id.iconsLayout)
+        for (i in iconsList.indices){
+            val imageView:ImageView = ImageView(requireContext())
+            imageView.setImageResource(iconsList[i].iconResources)
+            imageView.setOnClickListener {
+                Message.log("press on image")
+            }
+            iconsLayout.addView(imageView)
+        }
     }
 }
