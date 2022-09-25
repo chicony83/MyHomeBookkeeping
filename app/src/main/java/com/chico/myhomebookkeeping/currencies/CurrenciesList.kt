f275fbae00005a013e5719eb5a8f0fdb96d85440
chicony83 <chicony83@gmail.com>
2022-09-25 21:34:25 +0200

    add  empty CurrenciesList

---
+++ app/src/main/java/com/chico/myhomebookkeeping/currencies/CurrenciesList.kt
@@ -0,0 +1,9 @@
+package com.chico.myhomebookkeeping.currencies
+
+import com.chico.myhomebookkeeping.db.entity.Currencies
+
+class CurrenciesList {
+    private val currenciesList = listOf<Currencies>(
+        Currencies("BYN",null,false)
+    )
+}
