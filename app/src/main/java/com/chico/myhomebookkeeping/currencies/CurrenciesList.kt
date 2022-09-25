f8431ee69254a772bb2dedcc89477d1eb274d5e2
chicony83 <chicony83@gmail.com>
2022-09-25 21:34:47 +0200

    comment

--- app/src/main/java/com/chico/myhomebookkeeping/currencies/CurrenciesList.kt
+++ app/src/main/java/com/chico/myhomebookkeeping/currencies/CurrenciesList.kt
@@ -4,6 +4,6 @@ import com.chico.myhomebookkeeping.db.entity.Currencies
 
 class CurrenciesList {
     private val currenciesList = listOf<Currencies>(
-        Currencies("BYN",null,false)
+//        Currencies("BYN",null,false)
     )
 }
