e6c647c438f65a7ad2b64c9b08e8ea853853dbc0
chicony83 <chicony83@gmail.com>
2022-10-03 19:25:29 +0200

    add Currencies list

--- app/src/main/java/com/chico/myhomebookkeeping/currencies/CurrenciesList.kt
+++ app/src/main/java/com/chico/myhomebookkeeping/currencies/CurrenciesList.kt
@@ -4,6 +4,21 @@ import com.chico.myhomebookkeeping.db.entity.Currencies
 
 class CurrenciesList {
     private val currenciesList = listOf<Currencies>(
-//        Currencies("BYN",null,false)
+        Currencies("Azərbaycan manatı", "man.", "AZN", null, false),      //Азербальджан
+        Currencies("Հայկական դրամ", "֏", "AMD", null, false),          //Армения
+        Currencies("Беларускі рубель", "Br.", "BYN", null, false),     //Беларусь
+        Currencies("ლარი", "ლ", "GEL", null, false),                   //Грузия
+        Currencies("United States dollar", "\$", "USD", null, false),
+        Currencies("Euro", "€", "EUR", null, false),                    //Эстония,Латвия,Литва
+        Currencies("Теңге", "₸", "KZT", null, false),                   //Казахстан
+        Currencies("Сом", "с", "KGS", null, false),                      //Киргизия
+        Currencies("Leu moldovenesc", "L", "MDL", null, false),           //Молдавия
+        Currencies("Российский рубль", "₽", "RUB", null, false),           //Россия
+        Currencies("Сомонӣ", "смн.", "SM", null, false),                    //Таджикистан
+        Currencies("Türkmen Manaty", " \tm", "TMT", null, false),            //Туркменистан
+        Currencies("So‘m", "So'm", "UZS", null, false),                     //узбекистан
+        Currencies("Гривня", "₴", "UAH", null, false),                       //Украина
+        Currencies("Franc suisse", "₣", "CFH", null, false),                    //Швейцария
+        Currencies("Pound Sterling", "£", "GBP", null, false)
     )
 }
